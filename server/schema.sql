39c5b736b028db6011646aac9a92be13812d095a
Yang Chen <y.chen.pf@gmail.com>
2018-05-26 16:43:23 -0700

    add files and folderes table (#45)

--- server/schema.sql
+++ server/schema.sql
@@ -15,3 +15,34 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS `files`;
+
+CREATE TABLE files (
+  id integer AUTO_INCREMENT,
+  file_name varchar(200) NOT NULL,
+  file_ext varchar(200) NOT NULL,
+  folder_id integer NOT NULL,
+  created_by_user_id integer NOT NULL,
+  created_on date NOT NULL,
+  s3_objectId varchar(400) NOT NULL,
+  acl varchar(100) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS `folders`;
+
+CREATE TABLE folders (
+  id integer AUTO_INCREMENT,
+  folder_name varchar(200) NOT NULL,
+  parent_folderid integer NOT NULL,
+  full_path varchar(1000) NOT NULL,
+  acl varchar(100) NOT NULL,
+  created_by_user_id integer NOT NULL,
+  created_on date NOT NULL,
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE files ADD FOREIGN KEY (folder_id) REFERENCES folders (id);
+ALTER TABLE files ADD FOREIGN KEY (created_by_user_id) REFERENCES users (id);
+ALTER TABLE folders ADD FOREIGN KEY (created_by_user_id) REFERENCES users (id);
+
