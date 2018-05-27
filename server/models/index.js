1b7addbe2ee180a605471bf8e0b95fb6f7aa5a32
Yang Chen <y.chen.pf@gmail.com>
2018-05-26 18:00:17 -0700

    add createFile, createFolder function to model/index.js. Add timestamp to schema. (#48)
    * add createFile, createFolder function to model/index.js. Add timestamp
    to schema.
    
    * correct semicolon on sql statement and javascript statement;

--- server/models/index.js
+++ server/models/index.js
@@ -7,7 +7,8 @@ const createUser = (user, cb) => {
                 email = '${user.email}',
                 firstname = '${user.firstname}',
                 lastname = '${user.lastname}'
-                ;`
+                `;
+
   db.connection.query(query, function(error, results, fields) {
     if(error) {cb(error, null);}
     if(results) {cb(null, results);}
@@ -33,6 +34,41 @@ const fetchUser = (useremail, cb) => {
   })
 };
 
+const createFile = (file, cb) => {
+
+  var query = `INSERT INTO files SET
+                file_name = '${file.file_name}',
+                file_ext = '${file.file_ext}',
+                folder_id = '${file.folder_id}',
+                created_by_user_id = '${file.created_by_user_id}',
+                s3_objectId = '${file.s3_objectId}',
+                acl = '${file.acl}'
+                `;
+
+  db.connection.query(query, function(error, results, fields) {
+    if(error) {cb(error, null);}
+    if(results) {cb(null, results);}
+  });
+};
+
+const createFolder = (folder, cb) => {
+
+  var query = `INSERT INTO files SET
+                folder_name = '${folder.folder_name}',
+                parent_folderid = '${folder.parent_folderid}',
+                full_path = '${folder.full_path}',
+                created_by_user_id = '${folder.created_by_user_id}',
+                acl = '${folder.acl}'
+                `;
+
+  db.connection.query(query, function(error, results, fields) {
+    if(error) {cb(error, null);}
+    if(results) {cb(null, results);}
+  });
+};
+
 exports.fetchUser = fetchUser;
 exports.createUser = createUser;
 exports.checkUserExists = checkUserExists;
+exports.createFile = createFile;
+exports.createFolder = createFolder;
