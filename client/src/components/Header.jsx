70a1b0c8e9036eb1969a2f54fdbecfb9bd864db0
Joseph Gomez <jbgomez88@gmail.com>
2018-05-30 10:10:55 -0700

    Logo/update (#60)
    * Update logo file name
    
    * Update logo assets

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
-import logo from '../assets/logo-xs.png';
+import logo from '../assets/logo-sm.png';
 import {
   Navbar,
   NavbarBrand,
