f6292b56949d767a39f056d98152f4bb2452da26
Yang Chen <y.chen.pf@gmail.com>
2018-06-02 00:12:19 -0700

    add folder icon to path shown on UI (#77)

--- client/src/components/Path.jsx
+++ client/src/components/Path.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {Col, Row} from 'reactstrap';
+import folderIcon from '../assets/Folder.png';
 
 const Path = ({path}) => (
  <Row className="mt-3">
     <Col xs="10" sm="10" md="10" lg="10">
       {path.length > 0
-        ? path.slice(-3).map((folder) => (<a href="#">>{folder.name}</a>))
+        ? path.slice(-3).map((folder) => (
+            folder.name
+              ? <a href="#"> > <img width="30px" src={folderIcon} alt="folder icon"/>{folder.name}</a>
+              : <a href="#"><img width="30px" src={folderIcon} alt="folder icon"/>home</a>
+            )
+          )
         : null
       }
     </Col>
