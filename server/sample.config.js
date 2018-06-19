809feb84751a820d062c9d625d871e71a636c3ff
joseph <jbgomez88@gmail.com>
2018-06-18 21:02:37 -0700

    Update packages and scripts

--- server/sample.config.js
+++
@@ -1,7 +0,0 @@
-const keys = {
-  accessKeyId: process.env.ACCESS_KEY_ID || ACCESS_KEY_ID;,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY || SECRET_ACCESS_KEY,
-  region: process.env.REGION || REGION
-};
-
-exports.keys = keys;
